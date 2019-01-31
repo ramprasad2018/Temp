76d9cda78bdc3dd54c037e50290c8edae492734d
Ram Prasad Panigrahy <43319298+ramprasad2018@users.noreply.github.com>
2019-01-31 23:47:16 +0800

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,19 @@
+ADDON
+----------
+
+Commands
+----------
+node-gyp --python c:\Python27 configure
+hello.cc should be from 
+https://nodejs.org/api/addons.html
+
+using .so
+-----------
+https://stackoverflow.com/questions/50531237/node-gyp-c-import-shared-library-so
+"include_dirs": [
+  "../../src",
+  "/usr/local/include/RF24"
+],
+"libraries": [
+  "/usr/local/lib/librf24.so.1.3.1"
+]
