a2e043f59ad34cae2c083f904ee82b308c641e78
Ram Prasad Panigrahy <43319298+ramprasad2018@users.noreply.github.com>
2019-02-02 16:28:44 +0800

    Update Node.js

--- Node.js
+++ Node.js
@@ -17,3 +17,10 @@ https://stackoverflow.com/questions/50531237/node-gyp-c-import-shared-library-so
 "libraries": [
   "/usr/local/lib/librf24.so.1.3.1"
 ]
+
+How to retrive arguments
+-------------------------
+  
+  http://luismreis.github.io/node-bindings-guide/docs/getting-started.html
+  
+
